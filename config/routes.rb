67774075f96a78c7c8adfcfa84ad68b7a5b4368d
tomostock <tomos.tomomi@gmail.com>
2021-02-10 14:31:22 +0900

    routes.rb競合

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-<<<<<<< HEAD
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-=======
-
-  get  '/signup',  to: 'user#new'
->>>>>>> filling-in-layout
-=======
-
-  get  '/signup',  to: 'user#new'
->>>>>>> filling-in-layout
   root 'static_pages#home'
+  get  '/signup',  to: 'user#new'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-
 end
