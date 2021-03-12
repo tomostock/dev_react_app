0c4a4fa46a84aafc6054151ecdf194aa5dc02ac3
tomostock <tomos.tomomi@gmail.com>
2021-03-12 19:50:32 +0900

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -16,6 +16,8 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination'
     assert_select 'div.pagination', count: 1
+    assert_match @user.followers.count.to_s, response.body
+    assert_match @user.following.count.to_s, response.body
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
